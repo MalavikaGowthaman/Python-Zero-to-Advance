89b497d49b30cb3fb5746b0e966b293de4e814c9
Malavika Gowthaman <167611078+MalavikaGowthaman@users.noreply.github.com>
2024-09-11 19:46:43 -0400

    Add files via upload

---
+++ 23.f_string_function.py
@@ -0,0 +1,17 @@
+
+
+# name = 'Malavika'
+# gender ='Female'
+
+# s = "My name is {} and I am a {}"
+# word = s.format(name,gender)
+# print(word)
+
+
+
+
+name= 'Malavika'
+gender='Female'
+
+s = f"My name is {name} and I am a {gender}"
+print(s)
