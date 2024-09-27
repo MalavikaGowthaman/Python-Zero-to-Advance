3122a65f9c0e57f45819af871527670e339986e5
Malavika Gowthaman <167611078+MalavikaGowthaman@users.noreply.github.com>
2024-09-27 08:48:09 -0700

    Add files via upload

---
+++ 62.Virtual_env.py
@@ -0,0 +1,21 @@
+'''
+# to create separate env for a project
+
+# to create virtual env
+
+conda create -n name python == version -y
+
+# to check version
+python --version
+/
+python
+
+
+# to activate
+
+conda activate name
+/
+activate name
+
+
+'''
